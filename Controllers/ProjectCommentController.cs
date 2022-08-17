fae62cbe160305337a5ded78067aabdca93dd7cd
kvinay18 <105418879+kvinay18@users.noreply.github.com>
2022-08-17 16:23:33 +0530

    Add files via upload

---
+++ Controllers/ProjectCommentController.cs
@@ -0,0 +1,37 @@
+﻿using AenonSrp.Application.Features.ProjectComments.Commands.Delete;
+using AenonSrp.Application.Features.ProjectComments.Queries.GetAllByProjectId;
+using AenonSrp.Application.Features.ProjectComments.Queries.GetById;
+using AenonSrp.Application.Requests.Project;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AenonSrp.Server.Controllers.v1.Project
+{
+    public class ProjectCommentController : BaseApiController<ProjectDetailController>
+    {
+        [HttpGet("commentbyprojectid")]
+        public async Task<IActionResult> GetAllByProjectId(int projectId, int pageNumber, int pageSize)
+        {
+            var Checklists = await _mediator.Send(new GetAllProjectByProjectIdQuery() { ProjectId = projectId, PageNumber = pageNumber, PageSize = pageSize });
+            return Ok(Checklists);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var Checklist = await _mediator.Send(new GetProjectCommentByIdQuery() { Id = id });
+            return Ok(Checklist);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ProjectCommentRequest request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _mediator.Send(new DeleteProjectCommentCommand { Id = id }));
+        }
+    }
+}
---
+++ Controllers/ProjectTaskController.cs
@@ -0,0 +1,44 @@
+﻿using AenonSrp.Application.Features.ProjectTasks.Commands.Delete;
+using AenonSrp.Application.Features.ProjectTasks.Queries.GetAll;
+using AenonSrp.Application.Features.ProjectTasks.Queries.GetById;
+using AenonSrp.Application.Features.ProjectTasks.Queries.GetCountCalculation;
+using AenonSrp.Application.Requests.Project;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AenonSrp.Server.Controllers.v1.Project
+{
+    public class ProjectTaskController : BaseApiController<ProjectDetailController>
+    {
+        [HttpPost("taskbyprojectid")]
+        public async Task<IActionResult> GetAllByProjectId(ProjectTaskPagingRequest request)
+        {
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var response = await _mediator.Send(new GetProjectTaskByIdQuery() { Id = id });
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ProjectTaskRequest request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _mediator.Send(new DeleteProjectTaskCommand { Id = id }));
+        }
+
+        [HttpGet("gettaskcalculatedcount")]
+        public async Task<IActionResult> GetTaskCalculatedCount()
+        {
+            return Ok(await _mediator.Send(new GetProjectTaskCountCalculationQuery() { }));
+        }
+    }
+}
